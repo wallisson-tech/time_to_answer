5fba59752a27b95bed758087a41f885d0946f280
wallisson-tech <wallissonlopes.ti@gmail.com>
2024-12-29 11:06:26 -0300

    Ajustando o sistema de layouts (Site)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,9 +10,10 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
+
+  get 'inicio', to: 'site/welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root to: 'welcome#index'
+root to: 'site/welcome#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
